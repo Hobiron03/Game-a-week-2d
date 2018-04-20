9d58e626c63a9af69f37c909f5fc7041d9f8d0f4
MYCOMPUTER\yudai <yudainepnep@icloud.com>
2018-04-20 19:38:07 +0900

    ばねの追加

---
+++ Assets/SpringController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpringController : MonoBehaviour {
+
+    public GameObject springDefault;
+    public GameObject springRight;
+    public GameObject springLeft;
+
+    public enum SPEING_DIR
+    {
+        DEFAULT,
+        RIGHT,
+        LEFT,
+    }
+
+    private SPEING_DIR springDir = SPEING_DIR.DEFAULT;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		switch(springDir)
+        {
+            case SPEING_DIR.DEFAULT:
+                springDefault.SetActive(true);
+                break;
+        }
+	}
+}
