93b99fdc1802b498bcc8d4a89e3d4c9600f31898
MYCOMPUTER\yudai <yudainepnep@icloud.com>
2018-04-24 18:15:49 +0900

    雲が自動で動くようにした

---
+++ Assets/Scripts/CloudController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Translate(0.008f, 0, 0);
+        if (transform.position.x > 9.7f)
+        {
+            transform.position = new Vector3(-3.35f,-2.16f,0);
+        }
+	}
+}
